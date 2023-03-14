20998d7a21f74bdd302647991060f4c6e309ae5a
racooper99 <124007767+racooper99@users.noreply.github.com>
2023-03-14 18:56:26 -0400

    Initial Commit

---
+++ Piano/PianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoApp.swift
+//  Piano
+//
+//  Created by racooper on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
